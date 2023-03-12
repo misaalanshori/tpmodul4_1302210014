42b7e250f4984cb2112705a86d6aed1cb3ea6833
M Isa Al Anshori <isabungsu@gmail.com>
2023-03-12 08:54:04 +0700

    inisialisasi projek

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TPMODUL4_1302210014
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
